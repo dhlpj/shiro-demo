fd0a744f4cc8ed98170101847d2270fb0bac6edd
Jack <773411997@qq.com>
2018-10-25 15:15:37 +0800

    add some annotation to project

--- microservice-discovery-eureka/src/main/java/com/itmuch/cloud/study/EurekaApplication.java
+++ microservice-discovery-eureka/src/main/java/com/itmuch/cloud/study/EurekaApplication.java
@@ -2,6 +2,7 @@ package com.itmuch.cloud.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+//@EnableEurekaClient 只适用于服务发现组件Eureka,不能够针对的
 public class EurekaApplication {
   public static void main(String[] args) {
     SpringApplication.run(EurekaApplication.class, args);
